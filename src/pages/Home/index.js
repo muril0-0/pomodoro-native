12bd46cff8d2d982b6ba26565cd0a3e22de53717
Murilo Oliveira <muriloholiveira@hotmail.com>
2020-06-03 10:56:44 -0300

    Created style: Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,66 @@
 import React from "react";
-import { View } from "react-native";
+import {
+  ImageBackground,
+  StatusBar,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { AppLoading } from "expo";
+import { useFonts } from "@use-expo/font";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import styles from "./styles";
+import background from "../../assets/img/bg.png";
+
+let customFonts = {
+  "Quantico-Regular": require("../../assets/fonts/Quantico-Regular.ttf"),
+  "Quantico-Bold": require("../../assets/fonts/Quantico-Bold.ttf"),
+};
 
 export default function Home() {
+  [isLoaded] = useFonts(customFonts);
+
+  if (!isLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.topBar}></View>
-      <View style={styles.display}></View>
-      <View style={styles.controls}></View>
-    </View>
+    <ImageBackground source={background} style={styles.imgBG}>
+      <StatusBar
+        barStyle="light-content"
+        hidden={false}
+        backgroundColor="transparent"
+        translucent={true}
+        networkActivityIndicatorVisible={true}
+      />
+      <View style={styles.container}>
+        <View style={styles.topBar}>
+          <FontAwesome5 name="cog" size={40} color="#fff" />
+        </View>
+        <View style={styles.display}>
+          <Text style={[styles.textDisplay, { fontFamily: "Quantico-Bold" }]}>
+            25:00
+          </Text>
+        </View>
+        <View style={styles.controls}>
+          <TouchableOpacity
+            style={[styles.controlBtn, { backgroundColor: "#33FE94" }]}
+          >
+            <FontAwesome5 name="play" size={35} color="#fff" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.controlBtn, { backgroundColor: "#FFB053" }]}
+          >
+            <FontAwesome5 name="pause" size={35} color="#fff" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.controlBtn, { backgroundColor: "#FF5B5E" }]}
+          >
+            <FontAwesome5 name="stop" size={35} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,11 +2,58 @@ import { StyleSheet } from "react-native";
 import Constants from "expo-constants";
 
 export default StyleSheet.create({
+  imgBG: {
+    flex: 1,
+    width: null,
+    height: null,
+    backgroundColor: "#191919",
+  },
+
   container: {
     display: "flex",
     flexDirection: "column",
+    alignItems: "center",
+    marginTop: 30 + Constants.statusBarHeight,
   },
-  topBar: {},
-  display: {},
-  controls: {},
+  topBar: {
+    marginLeft: "auto",
+    marginRight: 20,
+    marginTop: 20,
+  },
+  display: {
+    width: 400,
+    height: 400,
+    marginTop: 50,
+
+    borderWidth: 20,
+    borderColor: "#FD1E62",
+    borderStyle: "solid",
+    borderRadius: 400 / 2,
+
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textDisplay: {
+    color: "#FD1E62",
+    fontSize: 400 / 5,
+  },
+  controls: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  controlBtn: {
+    width: 80,
+    height: 80,
+    marginHorizontal: 20,
+    marginTop: 30,
+    borderRadius: 10,
+
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
