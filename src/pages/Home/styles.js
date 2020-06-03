706f9531183bb66c1515a08cb8bd3428ba461610
Murilo Oliveira <muriloholiveira@hotmail.com>
2020-06-03 09:15:25 -0300

    Created route: Home

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+export default StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  topBar: {},
+  display: {},
+  controls: {},
+});
