b58573b889472d19a0d92607d969ee47b62f6c23
Murilo Oliveira <muriloholiveira@hotmail.com>
2020-06-04 11:03:59 -0300

    Created Focus Counter System

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   ImageBackground,
   StatusBar,
@@ -20,10 +20,36 @@ let customFonts = {
 };
 
 export default function Home() {
+  const [focus, setFocus] = useState(25 * 60);
+  const [pause, setPause] = useState(true);
   const navigation = useNavigation();
 
   function toMenu() {
     navigation.navigate("Menu");
+  }
+
+  useEffect(() => {
+    const initiate = setInterval(() => {
+      if (!pause) {
+        setFocus((f) => f - 1);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(initiate);
+    };
+  }, [pause]);
+
+  function startFocus() {
+    setPause(false);
+  }
+
+  function pauseFocus() {
+    setPause(true);
+  }
+
+  function stopFocus() {
+    setPause(true);
+    setFocus(25 * 60);
   }
 
   [isLoaded] = useFonts(customFonts);
@@ -49,22 +75,27 @@ export default function Home() {
         </View>
         <View style={styles.display}>
           <Text style={[styles.textDisplay, { fontFamily: "Quantico-Bold" }]}>
-            25:00
+            {`${Math.floor(focus / 60)}:${("00" + Math.floor(focus % 60)).slice(
+              -2
+            )}`}
           </Text>
         </View>
         <View style={styles.controls}>
           <TouchableOpacity
             style={[styles.controlBtn, { backgroundColor: "#33FE94" }]}
+            onPress={startFocus}
           >
             <FontAwesome5 name="play" size={35} color="#fff" />
           </TouchableOpacity>
           <TouchableOpacity
             style={[styles.controlBtn, { backgroundColor: "#FFB053" }]}
+            onPress={pauseFocus}
           >
             <FontAwesome5 name="pause" size={35} color="#fff" />
           </TouchableOpacity>
           <TouchableOpacity
             style={[styles.controlBtn, { backgroundColor: "#FF5B5E" }]}
+            onPress={stopFocus}
           >
             <FontAwesome5 name="stop" size={35} color="#fff" />
           </TouchableOpacity>
